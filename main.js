3fdcd2333ba2dd997684406c5996eeb970839ee9
mateusprojects111111 <antonio.ribeiro.mateus@escola.pr.gov.br>
2026-04-24 08:59:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,11 @@ for(let i=o;i <botoes.lenght;i++){
   botoes[i}.onclick = function(){
     
 for(let j=0;j<botoes.lenght;j++){
-  botoe[j].classList.remove("ativo");
+  botoes[j].classList.remove("ativo");
+  textos[j].classList.remove("ativo");
 }
     
     botoes[i}.classList.add("ativo");
+    textos{i}.classList.remove("ativo");
 }
   }
