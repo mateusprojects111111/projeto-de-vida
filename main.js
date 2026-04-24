5e00b180cce5a94783f87ec1999639ec4278f998
mateusprojects111111 <antonio.ribeiro.mateus@escola.pr.gov.br>
2026-04-24 08:54:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const botoes = document.quertSelectorAll(".botao");
-
+const textos = document.querySelectorAll(".aba-conteudo");
+  
 for(let i=o;i <botoes.lenght;i++){
   botoes[i}.onclick = function(){
     
